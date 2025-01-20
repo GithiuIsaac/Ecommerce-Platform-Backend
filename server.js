1262818174aebcc9a7555271f16030c9764e8505
igithiu <igithiu@gmail.com>
2025-01-20 16:01:38 +0300

    Used the cors package to fix the cors error

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // app.use with the ES6 import format, not the CommonJS one
 app.use("/api", authRoutes);
