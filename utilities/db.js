5811d238b8dd93f0bdfe4678174ed4a70d87452f
igithiu <igithiu@gmail.com>
2025-01-20 18:07:11 +0300

    Added dbConnect functionality

---
+++ utilities/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, { useNewURLParser: true });
+    console.log(`Database connected at ${process.env.DB_URL}`);
+  } catch (error) {
+    console.log("Database Connection Error", error.message);
+  }
+};
+
+export default dbConnect;
