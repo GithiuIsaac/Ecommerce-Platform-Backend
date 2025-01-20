51c3d60466f8074bad108fa9cbf3bd825f8f49f6
igithiu <igithiu@gmail.com>
2025-01-20 20:03:40 +0300

    helper function to create JWT tokens

--- utilities/tokenCreate.js
+++ utilities/tokenCreate.js
@@ -1,10 +1,8 @@
 import jwt from "jsonwebtoken";
 
-const createToken = async (data) => {
+export const createToken = async (data) => {
   const token = await jwt.sign(data, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
   return token;
 };
-
-export default createToken;
