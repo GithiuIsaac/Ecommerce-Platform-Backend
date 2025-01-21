e26f6020c7992e5496ab93ae557aaf55bc2058e8
igithiu <igithiu@gmail.com>
2025-01-22 00:38:39 +0300

    Added the get-user route, and configured it to use the authMiddleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authControllers from "../controllers/authControllers.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// router.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 router.post("/admin-login", authControllers.admin_login);
+router.get("/get-user", authMiddleware, authControllers.get_user);
 
 export default router;
