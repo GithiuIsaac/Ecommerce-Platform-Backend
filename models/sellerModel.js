5c483eb948b6a0bd69060dd52eb9fd388a40a574
igithiu <igithiu@gmail.com>
2025-02-05 23:56:55 +0300

    Defined sellers DB Model

---
+++ models/sellerModel.js
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+
+const sellerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      default: "seller",
+    },
+    status: {
+      type: String,
+      default: "pending",
+    },
+    paymentAccount: {
+      type: String,
+      default: "inactive",
+    },
+    method: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    sellerInfo: {
+      type: Object,
+      default: {},
+    },
+  },
+  { timestamps: true }
+);
+
+const sellerModel = model("sellers", sellerSchema);
+
+export default sellerModel;
