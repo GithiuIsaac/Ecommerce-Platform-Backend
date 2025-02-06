c0702b445a40b75e6469d9792d105492b91bfbbe
igithiu <igithiu@gmail.com>
2025-02-06 15:22:00 +0300

    Added the "/seller-login" endpoint, which uses the seller_login method defined in authControllers.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,7 @@ const router = Router();
 // });
 
 router.post("/admin-login", authControllers.admin_login);
+router.post("/seller-login", authControllers.seller_login);
 router.get("/get-user", authMiddleware, authControllers.get_user);
 router.post("/seller-register", authControllers.seller_register);
 export default router;
