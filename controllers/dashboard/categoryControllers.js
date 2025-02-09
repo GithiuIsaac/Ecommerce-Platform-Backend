38cda6424734bd17f22067ee7856c728bbd71730
igithiu <igithiu@gmail.com>
2025-02-10 01:56:54 +0300

    Implemented functionality to upload images to Cloudinary, return the result, and store the category data in the categories table

--- controllers/dashboard/categoryControllers.js
+++ controllers/dashboard/categoryControllers.js
@@ -1,30 +1,61 @@
 import formidable from "formidable";
 import categoryModel from "../../models/categoryModel.js";
 import { responseReturn } from "../../utilities/response.js";
-import { createToken } from "../../utilities/tokenCreate.js";
-import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
 
 class categoryControllers {
   add_category = async (req, res) => {
     const form = formidable();
     form.parse(req, async (err, fields, files) => {
-      console.log("Fields", fields);
-      console.log("Files", files);
       if (err) {
         responseReturn(res, 404, { error: "Sonething went wrong" });
       } else {
         let { category_name } = fields;
         let { image } = files;
-        name = category_name.trim();
-        const slug = name.split(" ").join("-").toLowerCase();
+        category_name = category_name[0].trim();
+        if (Array.isArray(files.image)) {
+          image = image[0];
+        }
+        if (!image || !image.filepath) {
+          return responseReturn(res, 400, { error: "Image file is required" });
+        }
+        const slug = category_name.split(" ").join("-").toLowerCase();
+
+        cloudinary.config({
+          cloud_name: process.env.cloud_name,
+          api_key: process.env.api_key,
+          api_secret: process.env.api_secret,
+          secure: true,
+        });
+
+        try {
+          console.log("Trying to upload the image...");
+          const result = await cloudinary.uploader.upload(image.filepath, {
+            folder: "categories",
+          });
+          if (result) {
+            const category = await categoryModel.create({
+              category_name,
+              slug,
+              image: result.secure_url,
+            });
+            responseReturn(res, 201, {
+              message: "Category added successfully",
+              category,
+            });
+          } else {
+            responseReturn(res, 400, { error: "Image upload failed" });
+          }
+        } catch (error) {
+          responseReturn(res, 500, { error: "Internal Server Error" });
+        }
       }
     });
-    console.log("New Category Created", req.body);
   };
 
   get_category = async (req, res) => {
     // Retrieve the category data from the DB
-    console.log();
+    console.log("Fetching categories from the DB...");
   };
 }
 export default new categoryControllers();
