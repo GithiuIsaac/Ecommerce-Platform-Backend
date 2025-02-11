8605d7f082b3392e512f3bdf29245a7892470ca8
igithiu <igithiu@gmail.com>
2025-02-12 01:39:23 +0300

    Added the product api endpoints

---
+++ routes/dashboard/productRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { authMiddleware } from "../../middleware/authMiddleware.js";
+import productControllers from "../../controllers/dashboard/productControllers.js";
+
+const router = Router();
+
+// The authMiddleware ensures no access for unauthenticated users
+router.get("/get-product", authMiddleware, productControllers.get_product);
+router.post("/add-product", authMiddleware, productControllers.add_product);
+
+export default router;
