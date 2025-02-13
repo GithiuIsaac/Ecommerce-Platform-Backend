0ef37defa22cfe7ed8b5a6e7cef9e9cfa5883bcd
igithiu <igithiu@gmail.com>
2025-02-13 23:35:42 +0300

    Added the /get-product route to product routes

--- routes/dashboard/productRoutes.js
+++ routes/dashboard/productRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 // The authMiddleware ensures no access for unauthenticated users
 router.get("/get-products", authMiddleware, productControllers.get_products);
+router.get("/get-product", authMiddleware, productControllers.get_product);
 router.post("/add-product", authMiddleware, productControllers.add_product);
 
 export default router;
