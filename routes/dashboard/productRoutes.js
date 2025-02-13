c74c22142671ae738c444d52b003276ca11db4ef
igithiu <igithiu@gmail.com>
2025-02-14 02:04:12 +0300

    Added the "/get-product/:productId" route to productRoutes

--- routes/dashboard/productRoutes.js
+++ routes/dashboard/productRoutes.js
@@ -6,7 +6,12 @@ const router = Router();
 
 // The authMiddleware ensures no access for unauthenticated users
 router.get("/get-products", authMiddleware, productControllers.get_products);
-router.get("/get-product", authMiddleware, productControllers.get_product);
+router.get(
+  "/get-product/:productId",
+  authMiddleware,
+  productControllers.get_product
+);
+// router.get("/get-product/:id", authMiddleware, productControllers.get_product);
 router.post("/add-product", authMiddleware, productControllers.add_product);
 
 export default router;
