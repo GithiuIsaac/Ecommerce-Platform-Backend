dda554a1e0d28227c4d01bbc1996e62a3b521bb6
igithiu <igithiu@gmail.com>
2025-03-10 01:48:18 +0300

    Added the get-products route

--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -5,11 +5,9 @@ import homeControllers from "../../controllers/home/homeControllers.js";
 const router = Router();
 
 // The authMiddleware ensures no access for unauthenticated users
-router.get(
-  "/get-categories",
-  // authMiddleware,
-  homeControllers.get_categories
-);
+router.get("/get-categories", homeControllers.get_categories);
+router.get("/get-products", homeControllers.get_products);
+
 // router.post("/add-category", authMiddleware, categoryControllers.add_category);
 
 export default router;
