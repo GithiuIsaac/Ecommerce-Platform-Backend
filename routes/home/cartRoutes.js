0076ce4cc804ecc2d68a1e0bd2f8e43a4dbae54a
igithiu <igithiu@gmail.com>
2025-03-17 03:01:23 +0300

    Added the get-cart-products route, and modified the get-product-count route to a get endpoint

--- routes/home/cartRoutes.js
+++ routes/home/cartRoutes.js
@@ -6,7 +6,8 @@ const router = Router();
 
 // The authMiddleware ensures no access for unauthenticated users
 router.post("/add-to-cart", cartControllers.add_to_cart);
-router.post("/get-product-count", cartControllers.get_product_count);
+router.get("/get-product-count", cartControllers.get_product_count);
+router.get("/get-cart-products/:customerId", cartControllers.get_cart_products);
 // router.get("/price-range-products", homeControllers.price_range_products);
 // router.get("/query-products", homeControllers.query_products);
 // router.post("/add-category", authMiddleware, categoryControllers.add_category);
