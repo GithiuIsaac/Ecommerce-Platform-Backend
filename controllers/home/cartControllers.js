b924fc4f296b0b28467abf6ff611ed2cc01bd9b9
igithiu <igithiu@gmail.com>
2025-03-19 19:23:26 +0300

    Added get_cart_products_opt to test a more efficient operation to return the cart products data

--- controllers/home/cartControllers.js
+++ controllers/home/cartControllers.js
@@ -114,9 +114,9 @@ class cartControllers {
     }
   };
 
+  // Retrieve and organize cart products for a specific customer
   get_cart_products = async (req, res) => {
     // Destructure customerId from the request params
-    // console.log(req.params);
     const { customerId } = req.params;
     try {
       // Find all cart items for a specific customer
@@ -292,6 +292,189 @@ class cartControllers {
         inStockQuantity,
         shippingFee: 500 * cartItemsBySeller.length,
       });
+    } catch (error) {
+      console.log(error.message);
+      responseReturn(res, 500, { error: error.message });
+    }
+  };
+
+  get_cart_products_opt = async (req, res) => {
+    // Destructure customerId from the request params
+    const { customerId } = req.params;
+    try {
+      // Find all cart items for a specific customer
+      // cartModel.aggregate - runs an aggregation pipeline on the cartModel collection
+      // $match stage - Filters documents where the customerId matches the provided customerId
+      // $eq: new ObjectId(customerId) - Converts customerId to an ObjectId and checks for equality with the provided customerId
+      const cartProducts = await cartModel.aggregate([
+        {
+          $match: {
+            customerId: {
+              $eq: new ObjectId(customerId),
+              // Returns all cart products for this user
+            },
+          },
+        },
+
+        // $lookup stage - Joins data from the 'products' collection to the cartModel collection
+        // Outputs a products_data array with an object which can be used to access all the fields in the "from" collection
+        {
+          $lookup: {
+            from: "products", // The collection to join
+            localField: "productId", // The field from the input documents
+            foreignField: "_id", // The field from the documents of the "from" collection
+            as: "products_data", // The output array field
+          },
+        },
+
+        // $unwind stage - Deconstructs an array field from the input documents to output a document for each element
+        {
+          $unwind: "$products_data",
+        },
+
+        // $project stage - Specifies the fields to include in the output documents
+        {
+          $project: {
+            _id: 1,
+            customerId: 1, // Include the customerId field
+            productId: 1, // Include the productId field
+            quantity: 1, // Include the quantity field
+            // product: "$products_data",
+            product_name: "$products_data.product_name",
+            seller_name: "$products_data.seller_name",
+            price: "$products_data.price",
+            brand: "$products_data.brand",
+            stock: "$products_data.stock",
+            discount: "$products_data.discount",
+            sellerId: "$products_data.sellerId",
+            image: { $arrayElemAt: ["$products_data.images", 0] },
+          },
+        },
+      ]);
+
+      console.log(cartProducts);
+
+      // Initialize counters
+      // let buy_product_item = 0; // Total quantity of in-stock items
+      // let calculatePrice = 0; // Total price of all in-stock items
+      // let cart_product_count = 0; // Number of products in the cart
+
+      // Split products into in-stock and out-of-stock
+      // products_data[0] - Access the fields in the products_data array specified above
+      // When the stock is less than the cart quantity, the product is out of stock
+      // const outOfStockProducts = cartProducts.filter(
+      //   (product) => product.products_data[0].stock < product.quantity
+      // );
+      // console.log("Out of stock:", outOfStockProducts);
+
+      // // Product is in stock if the stock quantity is greater than or equal to the cart quantity
+      // const stockProducts = cartProducts.filter(
+      //   (product) => product.products_data[0].stock >= product.quantity
+      // );
+      // // console.log("Products in stock:", stockProducts);
+
+      // // Calculate total quantity of out-of-stock items
+      // const outOfStockQuantity = outOfStockProducts.reduce(
+      //   (sum, product) => sum + product.quantity,
+      //   0
+      // );
+      // console.log("Out of stock quantity:", outOfStockQuantity);
+
+      // // Calculate total quantity of in stock items
+      // const inStockQuantity = stockProducts.reduce(
+      //   (sum, product) => sum + product.quantity,
+      //   0
+      // );
+      // console.log("Products in stock quantity:", inStockQuantity);
+
+      // // Group products by seller
+      // let cartItemsBySeller = [];
+      // // Get unique seller IDs - creates an array of unique seller IDs
+      // let uniqueSellerIds = [
+      //   ...new Set(
+      //     stockProducts.map((p) => p.products_data[0].sellerId.toString())
+      //   ),
+      // ];
+
+      // // loops through each unique seller
+      // for (let i = 0; i < uniqueSellerIds.length; i++) {
+      //   // Total price for each seller
+      //   let sellerTotal = 0;
+      //   // Loop through all products - checks each product to see if it belongs to the current seller being processed.
+      //   for (let j = 0; j < stockProducts.length; j++) {
+      //     const tempProduct = stockProducts[j].products_data[0];
+      //     // Check if product belongs to current seller
+      //     if (tempProduct.sellerId.toString() === uniqueSellerIds[i]) {
+      //       // Calculate price for this product with discount if applicable
+      //       const productTotal = this.computePrice(
+      //         tempProduct.price,
+      //         stockProducts[j].quantity,
+      //         tempProduct.discount
+      //       );
+
+      //       // Add to seller's total
+      //       sellerTotal += productTotal;
+
+      //       // Group products by seller
+      //       cartItemsBySeller[i] = {
+      //         sellerId: uniqueSellerIds[i],
+      //         sellerName: tempProduct.seller_name,
+      //         price: sellerTotal + this.computeCommission(sellerTotal),
+      //         products_data: cartItemsBySeller[i]
+      //           ? [
+      //               // If seller already has products
+      //               ...cartItemsBySeller[i].products_data,
+      //               // Keep existing products, append new product
+      //               {
+      //                 //
+      //                 _id: stockProducts[j]._id,
+      //                 // product_name: stockProduct[j].products_data[0].product_name,
+      //                 // brand: stockProduct[j].products_data[0].brand,
+      //                 quantity: stockProducts[j].quantity,
+      //                 // price: stockProduct[j].products_data[0].price,
+      //                 // discount: stockProduct[j].products_data[0].discount,
+      //                 // image: stockProduct[j].products_data[0].images[0]
+      //                 productInfo: tempProduct,
+      //                 productTotal,
+      //               },
+      //             ]
+      //           : [
+      //               // If first product for this seller
+      //               {
+      //                 _id: stockProducts[j]._id,
+      //                 // product_name: stockProduct[j].products_data[0].product_name,
+      //                 // brand: stockProduct[j].products_data[0].brand,
+      //                 quantity: stockProducts[j].quantity,
+      //                 // price: stockProduct[j].products_data[0].price,
+      //                 // discount: stockProduct[j].products_data[0].discount,
+      //                 // image: stockProduct[j].products_data[0].images[0]
+      //                 productInfo: tempProduct,
+      //                 productTotal,
+      //               },
+      //             ],
+      //       };
+      //     }
+      //   }
+      // }
+
+      // // Calculate total price from seller groups
+      // const totalPrice = cartItemsBySeller.reduce(
+      //   (sum, seller) => sum + seller.price,
+      //   0
+      // );
+
+      // console.log("Products grouped by seller", cartItemsBySeller);
+
+      // console.log("The calculated price is:", totalPrice);
+
+      // responseReturn(res, 200, {
+      //   cartProducts: cartItemsBySeller,
+      //   outOfStockProducts,
+      //   cartTotalPrice: totalPrice,
+      //   cartProductCount: inStockQuantity + outOfStockQuantity,
+      //   inStockQuantity,
+      //   shippingFee: 500 * cartItemsBySeller.length,
+      // });
     } catch (error) {
       console.log(error.message);
       responseReturn(res, 500, { error: error.message });
--- routes/home/cartRoutes.js
+++ routes/home/cartRoutes.js
@@ -8,6 +8,7 @@ const router = Router();
 router.post("/add-to-cart", cartControllers.add_to_cart);
 router.get("/get-product-count", cartControllers.get_product_count);
 router.get("/get-cart-products/:customerId", cartControllers.get_cart_products);
+// router.get("/get-cart-products/:customerId", cartControllers.get_cart_products_opt);
 // router.get("/price-range-products", homeControllers.price_range_products);
 // router.get("/query-products", homeControllers.query_products);
 // router.post("/add-category", authMiddleware, categoryControllers.add_category);
