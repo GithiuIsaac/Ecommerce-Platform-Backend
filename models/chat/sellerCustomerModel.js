7408a7afeaaacc3352028a3ec6395d2a5f526923
igithiu <igithiu@gmail.com>
2025-04-10 02:07:11 +0300

    Updated the sellerCustomerModel to more closely reflect the functionality. The userId can either be a sellerId or a customerId, and the linkedUsers array includes either sellers or customers, depending on the record's user type

--- models/chat/sellerCustomerModel.js
+++ models/chat/sellerCustomerModel.js
@@ -3,11 +3,19 @@ import { Schema, model } from "mongoose";
 
 const sellerCustomerSchema = new Schema(
   {
-    sellerId: {
+    userId: {
+      // can be either sellerId or customerId
       type: String,
       required: true,
     },
-    customerIds: {
+    userType: {
+      // to identify if this record belongs to a seller or customer
+      type: String,
+      enum: ["seller", "customer"],
+      required: true,
+    },
+    linkedUsers: {
+      // array of connected users (either sellers or customers)
       type: Array,
       default: [],
     },
