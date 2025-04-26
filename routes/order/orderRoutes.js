bce0c529edc66d0b0a61c057691b64cd90da4ef0
igithiu <igithiu@gmail.com>
2025-04-26 21:03:42 +0300

    Implemented the create_payment function and associated route to create the payment intent on stripe, and to return the clientSecret to the frontend. Edited the commit to remove the test stripe key

--- routes/order/orderRoutes.js
+++ routes/order/orderRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 // The authMiddleware ensures no access for unauthenticated users
 router.post("/place-order", orderControllers.place_order);
+router.post("/create-payment", orderControllers.create_payment);
 // router.get("/get-product-count", cartControllers.get_product_count);
 
 export default router;
