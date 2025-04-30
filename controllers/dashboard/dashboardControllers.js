3ef506720a83f22220093b468c54dd06e81641eb
igithiu <igithiu@gmail.com>
2025-04-30 13:30:08 +0300

    Implemented the get_seller_dashboard method which fetches data to be displayed on the seller dashboard.

--- controllers/dashboard/dashboardControllers.js
+++ controllers/dashboard/dashboardControllers.js
@@ -4,9 +4,11 @@ import customerOrderModel from "../../models/customerOrderModel.js";
 import shopWalletModel from "../../models/payment/shopWalletModel.js";
 import sellerModel from "../../models/sellerModel.js";
 import adminSellerMsgModel from "../../models/chat/adminSellerMessages.js";
+import sellerCustomerMsgModel from "../../models/chat/sellerCustomerMessages.js";
 import { responseReturn } from "../../utilities/response.js";
-
+import sellerWalletModel from "../../models/payment/sellerWalletModel.js";
 import mongoose from "mongoose";
+
 const { ObjectId } = mongoose.mongo;
 
 class dashboardControllers {
@@ -54,6 +56,90 @@ class dashboardControllers {
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  get_seller_dashboard = async (req, res) => {
+    const { id } = req;
+    // console.log(id);
+    try {
+      const totalSales = await sellerWalletModel.aggregate([
+        {
+          $match: {
+            sellerId: {
+              $eq: id,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            totalAmount: { $sum: "$amount" },
+          },
+        },
+      ]);
+      // console.log("Total Sales Amount: ", totalSales[0]?.totalAmount);
+
+      const totalProducts = await productModel
+        .find({
+          sellerId: new ObjectId(id),
+        })
+        .countDocuments();
+      // console.log("Total Products: ", totalProducts);
+
+      const totalOrders = await adminOrderModel
+        .find({
+          sellerId: new ObjectId(id),
+        })
+        .countDocuments();
+      // console.log("Total Orders: ", totalOrders);
+
+      const pendingOrders = await adminOrderModel
+        .find({
+          $and: [
+            // the $and operator combines two conditions that must both be true
+            {
+              sellerId: {
+                $eq: new ObjectId(id),
+              },
+            },
+            {
+              delivery_status: {
+                $in: ["pending_payment", "pending", "processing"],
+              },
+            },
+          ],
+        })
+        .countDocuments();
+      // Simplified version
+      // const pendingOrders = await adminOrderModel.find({
+      //   sellerId: new ObjectId(id),
+      //   delivery_status: { $in: ["pending_payment", "pending", "processing"] }
+      // });
+      // console.log("Pending Orders: ", pendingOrders);
+
+      // Recent customer messages sent to the current seller
+      const recentMessages = await sellerCustomerMsgModel
+        .find({
+          receiverId: id,
+        })
+        .limit(3)
+        .sort({ createdAt: -1 });
+      // console.log("Recent Messages: ", recentMessages);
+
+      const recentOrders = await adminOrderModel
+        .find({ sellerId: new ObjectId(id) })
+        .limit(5);
+      // console.log("Recent Orders: ", recentOrders);
+
+      responseReturn(res, 200, {
+        totalSalesAmount: totalSales[0]?.totalAmount,
+        totalProducts,
+        totalOrders,
+        pendingOrders,
+        recentMessages,
+        recentOrders,
+      });
+    } catch (error) {}
   };
 }
 export default new dashboardControllers();
